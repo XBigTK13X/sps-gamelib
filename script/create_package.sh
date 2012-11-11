d2afbceb8043c5795d7f93a1adb8b5ec34b08146
XBigTK13X <xbigtk13x@gmail.com>
2012-11-11 15:35:43 -0500

    Include README and default assets.

--- script/create_package.sh
+++ script/create_package.sh
@@ -4,5 +4,5 @@ mvn -q clean
 mvn -q package
 cd target
 git log --max-count=1 > version.txt
-zip -r sps-gamelib.zip sps-gamelib.jar assets version.txt README.txt 
+zip -r sps-gamelib.zip sps-gamelib.jar ../assets version.txt ../README.md 
 cd ../..
