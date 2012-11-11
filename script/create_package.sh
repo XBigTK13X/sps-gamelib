e2b4fe8ac16d8ec8b47722d15960270413ffe942
XBigTK13X <xbigtk13x@gmail.com>
2012-11-11 15:38:19 -0500

    Correcting the symlink

--- script/create_package.sh
+++ script/create_package.sh
@@ -4,5 +4,7 @@ mvn -q clean
 mvn -q package
 cd target
 git log --max-count=1 > version.txt
-zip -r sps-gamelib.zip sps-gamelib.jar ../assets version.txt ../README.md 
+ln ../assets ./assets
+ln ../README.md ./README.md
+zip -r sps-gamelib.zip sps-gamelib.jar assets version.txt README.md 
 cd ../..
