38be8f75b3c44f4f8829a9c2f4fbfe14dbaa1c8e
XBigTK13X <xbigtk13x@gmail.com>
2014-01-23 13:51:26 -0600

    BoundingBox for LightEntity.

--- src/sps/entities/LightEntity.java
+++ src/sps/entities/LightEntity.java
@@ -6,22 +6,32 @@ import sps.bridge.DrawDepth;
 import sps.bridge.DrawDepths;
 import sps.core.Point2;
 import sps.display.Window;
+import sps.util.BoundingBox;
 
 public class LightEntity {
     private Point2 _position;
     private Sprite _sprite;
     private boolean _active;
     private DrawDepth _depth;
+    private BoundingBox _bounds;
 
     public LightEntity() {
         LightEntities.get().add(this);
         _position = new Point2(0, 0);
         _depth = DrawDepths.get("Default");
         _active = true;
+        _bounds = BoundingBox.empty();
+    }
+
+    private void resetBounds() {
+        if (_sprite != null) {
+            BoundingBox.fromDimensions(_bounds, _position.X, _position.Y, (int) _sprite.getWidth(), (int) _sprite.getHeight());
+        }
     }
 
     public void setSprite(Sprite sprite) {
         _sprite = sprite;
+        resetBounds();
     }
 
     public void setActive(boolean active) {
@@ -33,10 +43,15 @@ public class LightEntity {
         if (_sprite != null) {
             _sprite.setPosition(x, y);
         }
+        resetBounds();
     }
 
     public Point2 getPosition() {
         return _position;
+    }
+
+    public BoundingBox getBounds() {
+        return _bounds;
     }
 
     public void setDrawDepth(DrawDepth depth) {
