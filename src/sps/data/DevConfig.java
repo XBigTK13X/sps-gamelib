6114c99fb1c1cf31ac20ac779480f100d3ce7ad0
XBigTK13X <xbigtk13x@gmail.com>
2014-01-23 12:03:47 -0600

    Dev config toggle logic

--- src/sps/data/DevConfig.java
+++ src/sps/data/DevConfig.java
@@ -1,6 +1,16 @@
 package sps.data;
 
+import sps.core.Loader;
+
 public class DevConfig {
+    private static Boolean __canEnable;
+    private static boolean canEnable() {
+        if (__canEnable == null) {
+            __canEnable = !Loader.get().data("release_build").exists();
+        }
+        return __canEnable;
+    }
+
     public static boolean EndToEndStateTest = false;
     public static boolean BotEnabled = false;
     public static boolean TimeStates = false;
